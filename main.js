e2a7eb8b7f7b485a43ec355c2ca69de3da7d294f
Abdulaziz780 <akhusanov410@gmail.com>
2024-04-16 15:15:03 +0500

    qiyin

---
+++ main.js
@@ -0,0 +1,58 @@
+//1
+// let son1 = 5
+// let son2 = 2
+
+// alert(son1 % son2)
+
+
+//2
+// let mathmetods = Math.round(Math.random() * 10)
+// alert(mathmetods)
+
+//3 
+// let son = 12.510
+// alert(Math.floor(son))
+
+//4
+// let str = 'MARS IT SCHOOL'
+// let str1 = str.length
+// alert(str1)
+
+//5
+// let str = 'MARS IT SCHOOL'
+// let str1 = str.repeat(10)
+// console.log(str1);
+
+//6
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+// console.log((harflar[1]) + (harflar[2]) + (harflar[3]) + (harflar[20]) + (harflar[11])); 
+
+//7 
+// let yosh = prompt('Yoshingizni kiriting')
+
+// if(yosh > 18){
+//     console.log('Siz balog`yoshiga yetingiz');
+// }
+
+// if(18 > yosh) {
+//     console.log('Siz hali pakanasiz');
+// }
+
+// if(18 == yosh){
+//     console.log('balogat yoshi muborak');
+// }
+
+//8
+// let ism = prompt('ism kiriting')
+// console.log(ism.split('').reverse().join('d'));
+
+//9
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr2 = []
+// console.log(arr);
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         arr2.push(arr[i]);
+//     }
+// }
+// console.log(arr2);
